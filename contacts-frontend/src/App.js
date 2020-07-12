04243aa8182503f5cb68eaea5f761e061a3b9f4e
Renee Thomsen <rcthomsen19@gmail.com>
2020-07-11 22:45:43 -0400

    created component and function

--- contacts-frontend/src/App.js
+++ contacts-frontend/src/App.js
@@ -1,54 +1,30 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
-import * as ContactsAPI from './utils/ContactsAPI'
+
+const contacts = [
+  {
+    "id": "erik",
+    "name": "Erik Thomsen",
+    "email": "etho201@gmail.com",
+    "avartarURL": "http://localhost:5001/erik.jpg"
+  },
+  {
+    "id": "renee",
+    "name": "Renee Thomsen",
+    "email": "rcthomsen19@gmail.com",
+    "avartarURL": "http://localhost:5001/renee.jpg"
+  }
+]
 
 class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
-  }
+//pass contact array to this listcontacts component and to access the array go to listcontacts js to add console...
 
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
+          <ListContacts contacts={contacts}/>
       </div>
     )
   }
 }
-
-export default App;
+export default App
--- contacts-frontend/src/ListContacts.js
+++ contacts-frontend/src/ListContacts.js
@@ -1,83 +1,31 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
+import React,{Component} from 'react'
+//props is an object and it has one property contacts which is the array of contacts
+// class ListContacts extends Component {
+    // render() {
+        // console.log('Props', this.props)
 
-class ListContacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-  }
-
-  state = {
-    query: ''
-  }
-
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-  }
-
-  clearQuery = () => {
-    this.setState({ query: '' })
-  }
-
-  render() {
-    const { contacts, onDeleteContact } = this.props
-    const { query } = this.state
-
-    let showingContacts
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i')
-      showingContacts = contacts.filter((contact) => match.test(contact.name))
-    } else {
-      showingContacts = contacts
-    }
-
-    showingContacts.sort(sortBy('name'))
-
-    return (
-      <div className='list-contacts'>
-        <div className='list-contacts-top'>
-          <input
-            className='search-contacts'
-            type='text'
-            placeholder='Search contacts'
-            value={query}
-            onChange={(event) => this.updateQuery(event.target.value)}
-          />
-          <Link
-            to='/create'
-            className='add-contact'
-          >Add Contact</Link>
-        </div>
-
-        {showingContacts.length !== contacts.length && (
-          <div className='showing-contacts'>
-            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
-            <button onClick={this.clearQuery}>Show all</button>
-          </div>
-        )}
-
-        <ol className='contact-list'>
-          {showingContacts.map((contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <div className='contact-avatar' style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}/>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
+        //instead of creating a class as shown in the code above, you can put the entire code in a function - stateless functional component
+        //use this when all the component needs is a `render` method
+        //since we pass props as the argument, "this" keyword can be taken out
+        function ListContacts(props) {
+            return(     // a javascript property that allows to modify each item in the array and returns a new array
+             <ol className='contact-list'>
+             {props.contacts.map((contact) => (
+                //UI for each item below;//whenever you use map function a key prop needs to be used
+                <li key={contact.id} className='contact-list-item'> 
+                <div className='contact-avatar' style={{
+                    backgroundImage: `url(${contact.avartarURL})`
+                }}/>  
+                <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.email}</p>
+                </div>
+                <button className='contact-remove'>
                 Remove
-              </button>
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
-  }
-}
-
+                </button>
+                </li>
+            ))}  
+            </ol>
+        )
+    }
 export default ListContacts
