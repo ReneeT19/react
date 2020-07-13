10ed4a041c3f6d6d6a11160dfa0dad2ab0a38c85
Renee Thomsen <rcthomsen19@gmail.com>
2020-07-12 22:27:03 -0400

    added remove contact button

--- contacts-frontend/src/App.js
+++ contacts-frontend/src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 
-const contacts = [
-  {
-    "id": "erik",
-    "name": "Erik Thomsen",
-    "email": "etho201@gmail.com",
-    "avartarURL": "http://localhost:5001/erik.jpg"
-  },
-  {
-    "id": "renee",
-    "name": "Renee Thomsen",
-    "email": "rcthomsen19@gmail.com",
-    "avartarURL": "http://localhost:5001/renee.jpg"
-  }
-]
-
+ 
 class App extends Component {
 //pass contact array to this listcontacts component and to access the array go to listcontacts js to add console...
+  state = {
+    contacts: [
+      {
+        "id": "erik",
+        "name": "Erik Thomsen",
+        "email": "etho201@gmail.com",
+        "avartarURL": "http://localhost:5001/erik.jpg"
+      },
+      {
+        "id": "renee",
+        "name": "Renee Thomsen",
+        "email": "rcthomsen19@gmail.com",
+        "avartarURL": "http://localhost:5001/renee.jpg"
+      }
+    ]
+  }
+  
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+
 
   render() {
     return (
       <div>
-          <ListContacts contacts={contacts}/>
+          <ListContacts 
+          onDeleteContact={this.removeContact} 
+          contacts={this.state.contacts}/>
       </div>
     )
   }
