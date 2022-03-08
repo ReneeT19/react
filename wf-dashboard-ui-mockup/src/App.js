acd9e570c2420b9ba058ef8213ba2b51d5ea3e8e
Renee Thomsen <rcthomsen19@gmail.com>
2022-03-07 22:08:44 -0500

    practice

--- wf-dashboard-ui-mockup/src/App.js
+++ wf-dashboard-ui-mockup/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dashboard Project Practice
         </a>
       </header>
     </div>
