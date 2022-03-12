839e741bf43560d0036929d393cc5bf994f6b95f
Renee Thomsen <rcthomsen19@gmail.com>
2022-03-11 22:12:25 -0500

    added JSX

--- wf-dashboard-ui-mockup/src/index.js
+++ wf-dashboard-ui-mockup/src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// example to use createElement
+// ReactDOM.render(
+//   React.createElement("h1", {style:{color:"red"}}, "Hello!"),
+//   document.getElementById('root')
+// );
+
+// example to use JSX
+// ReactDOM.render(
+//   <ul>
+//     <li>Hot Pot</li>
+//     <li>Hot Dogs</li>
+//     <li>Hot Soup</li>
+//   </ul>,
+//   document.getElementById('root')
+// );
+
+// JSX - create a variable and use it dynamically
+// let city = "Shanghai";
+// ReactDOM.render(
+//   <h1>Hello from {city}</h1>,
+//   document.getElementById('root')
+// );
+
+// JSX - create an object and use it dynamically
+let city = {
+  name: "Shanghai",
+  country: "China"
+};
 ReactDOM.render(
-  React.createElement("h1", {style:{color:"red"}}, "Hello!"),
+  <h1 id="heading" className="cool-text">{city.name} is in {city.country}</h1>,
   document.getElementById('root')
 );
