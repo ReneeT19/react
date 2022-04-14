0bc2015f70ce65127f8d8c28551674ef5f0c4b61
Renee Thomsen <rcthomsen19@gmail.com>
2022-04-14 12:01:23 -0400

    use Enum to improve code

--- react-redux-typescript/src/state/reducers/repositoriesReducer.ts
+++ react-redux-typescript/src/state/reducers/repositoriesReducer.ts
@@ -6,41 +6,51 @@ interface RepositoriesState {
 }
 
 //replace `action: any` with interface; action is an object that must have a type property and optionally a payload
-interface Action {
-    type: string;
-    payload?: any; //optional
-}
+// interface Action {
+//     type: string;
+//     payload?: any; //optional
+// }
 
 //comment out the code above and seperate interfaces for action based on the design
 //replace `action: Action` with the separate interfaces created
 interface SearchRepositoriesAction {
-    type: 'search_repositories';
+    type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSuccessAction {
-    type: 'search_repositories_success';
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
     payload: string[];
 }
 
 interface SearchRepositoriesErrorAction {
-    type: 'search_repositories_error';
+    type: ActionType.SEARCH_REPOSITORIES_ERROR;
     payload: string;
 }
 
+//to further improve the code, create a type for Action instead of passing the long code inside reducer
+type Action = 
+SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
+
+//to further improve the code, use Enum instead of passing the long string to switch function and interfaces too
+enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
 
 //pass interface annotation to the reducer - using TS to check value type (for example, data has to be string[] after adding the annotation)
 const reducer = 
     (state: RepositoriesState, 
-    action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction): 
+    action: Action): 
     RepositoriesState => {
 //usually use switch statement within reducer
     switch(action.type) { //if typing 'action.tes' TS will show error because the interface defined only has type, same as payload below
         //different cases are different action types defined in the design
-        case 'search_repositories':
+        case ActionType.SEARCH_REPOSITORIES:
             return { loading: true, error: null, data: []} //new search so data is empty array
-        case 'search_repositories_success':
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
             return { loading: false, error: null, data: action.payload }; //action.payload is the repos found from search
-        case 'search_repositories_error':
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
             return { loading: false, error: action.payload, data: []}; //action.payload is the error message from api; reset data to empty array
         default: 
             return state; 
