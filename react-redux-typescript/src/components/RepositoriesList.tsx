7db91afecfef6787625a7bb68ff430683eabed1e
Renee Thomsen <rcthomsen19@gmail.com>
2022-04-21 22:23:22 -0400

    set event listener and called action creators

--- react-redux-typescript/src/components/RepositoriesList.tsx
+++ react-redux-typescript/src/components/RepositoriesList.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
+const [term, setTerm] = useState('');
+const { searchRepositories }= useActions();
+//access reduc functions; call this function and provide whatever action creators you want to load such as Search Repo
+
+//the type of event can be found by hovering the mouse to onSubmit in line 13 but remove Handler keyword
+//set up correct type for the callback event
+const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); //to prevent React from refreshing the page whenever we click Search
+
+    searchRepositories(term);
+};
+
+//set up event listener on the form
     return (<div>
-        <form>
-            <input />
+        <form onSubmit={onSubmit}>
+            <input value={term} onChange={e => setTerm(e.target.value)}/>
             <button>Search</button>
         </form>
     </div>
-
     );
 };
 
