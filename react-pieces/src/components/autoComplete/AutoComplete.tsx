78231803cdf41768a38245704c0fb8640fc3422d
Renee Thomsen <rcthomsen19@gmail.com>
2022-08-10 21:48:58 -0400

    successfully fetched input data for other use case

--- react-pieces/src/components/autoComplete/AutoComplete.tsx
+++ react-pieces/src/components/autoComplete/AutoComplete.tsx
@@ -1,33 +1,49 @@
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField';
-import React from 'react'
+import React, { useState } from 'react'
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 
 const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 },
+  { label: 'The Shawshank Redemption', year: 1994 },
+  { label: 'The Godfather', year: 1972 },
+  { label: 'The Godfather: Part II', year: 1974 },
+  { label: 'The Dark Knight', year: 2008 },
+  { label: '12 Angry Men', year: 1957 },
+  { label: "Schindler's List", year: 1993 },
+  { label: 'Pulp Fiction', year: 1994 },
 ];
 
-const AutoComplete = () => {
+ const AutoComplete = () => {
+
+  interface inputData {
+    label: string;
+    year: number | null;
+  }
+
+  const [input, setInput] = useState<inputData | null>();
+  const label = input?.label;
+  const year = input?.year;
+  console.log(label + " " + year);
+
+  const handleChange = (event: React.SyntheticEvent<Element, Event>, value: any) => {
+    setInput(value);
+  }
+
   return (
     <>
-    <h2>Demo of Autocomplete from MUI material</h2>
-    
+    <h3>Demo of Autocomplete from MUI material</h3>
     <Autocomplete
       disablePortal
       id="combo-box-demo"
       options={top100Films}
       sx={{ width: 300 }}
       popupIcon={<KeyboardArrowDown />}
+      onChange={handleChange}
       renderInput={(params) => <TextField {...params} label="Movie" />}
     />
+    <p>The name is {label} and the year is {year} </p>
     </>
   )
 }
 
-export default AutoComplete
+export default AutoComplete;
