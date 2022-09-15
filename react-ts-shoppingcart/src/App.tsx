ce8fe2e097e885877a549371d6b97a23ec1e28fd
Renee Thomsen <rcthomsen19@gmail.com>
2022-09-14 22:01:15 -0400

    added pages and navbar component

--- react-ts-shoppingcart/src/App.tsx
+++ react-ts-shoppingcart/src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
+import { Store } from './pages/Store';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
 
 function App() {
-  return <Container>
+  return (<>
+  <Navbar />
+    <Container className="mb-4">
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/store" element={<Store />} />
       <Route path="/about" element={<About />} />
     </Routes>
   </Container>
+  </>)
 }
 
 export default App;
