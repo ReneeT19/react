cb0f2e1cb338f4948ad4b32e83712c3efa48a325
Renee Thomsen <rcthomsen19@gmail.com>
2022-09-17 20:57:21 -0400

    added images and created StoreItem component

--- react-ts-shoppingcart/src/pages/Store.tsx
+++ react-ts-shoppingcart/src/pages/Store.tsx
@@ -1,4 +1,19 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
 export function Store() {
-    return <h1>Store</h1>
+    return <><h1>Store</h1>
+    {/* md means medium screen size has column of 2; g-3 means gap of 3 between items */}
+    <Row md={2} xs={1} lg={3} className="g-3"> 
+    {storeItems.map(item => (
+        // <Col>{JSON.stringify(item)}</Col>  This is to print the data info on the browser
+        // takes in the item properties such as name, pricing, etc.
+        <Col key={item.id}>
+            <StoreItem {...item} />
+        </Col>
+    ))}
+    </Row>
+
+</>
 }
